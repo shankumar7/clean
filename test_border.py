3319c883144916209fb27e867b0fe21d2e2b8f14
shankumar7 <shankumarpitta714@gmail.com>
2026-08-07 12:57:03 +0530

    chore: remove temporary QR generation scripts and test assets

--- test_border.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-qr = qrcode.QRCode(version=2, error_correction=qrcode.constants.ERROR_CORRECT_L, border=0)
-qr.add_data("http://172.29.98.183:5000")
-qr.make(fit=True)
-print(len(qr.modules))
